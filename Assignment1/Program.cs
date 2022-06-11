381eb785d05c684c58981be0553426c28bff2895
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-06-11 16:48:50 -0400

    working on dividing just need to prioritxze bugger note

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -23,35 +23,35 @@ for (int i = 0; i < result; i++)
     if (result % num20 == 0)
     {
         int resultDividebyTwo = result / num20;
-        Console.WriteLine($"So I should return you $20 bill.");
+        Console.WriteLine($"So I should return you {resultDividebyTwo} $20 bill.");
         break;
     }
 
     if (result % num10 == 0)
     {
         int resultDividebyTwo = result / num10;
-        Console.WriteLine($"So I should return you $10 bill.");
+        Console.WriteLine($"So I should return you {resultDividebyTwo} $10 bill.");
         break;
     }
 
     if (result % num5 == 0)
     {
         int resultDividebyTwo = result / num5;
-        Console.WriteLine($"So I should return you $5 bill.");
+        Console.WriteLine($"So I should return you {resultDividebyTwo} $5 bill.");
         break;
     }
 
     if (result % num2 == 0)
     {
         int resultDividebyTwo = result / num2;
-        Console.WriteLine($"So I should return you Coins of $2");
+        Console.WriteLine($"So I should return you {resultDividebyTwo} Coins of $2");
         break;
     }
 
     if (result % num1 == 0)
     {
         int resultDividebyTwo = result / num1;
-        Console.WriteLine($"So I should return you Coins of $1");
+        Console.WriteLine($"So I should return you {resultDividebyTwo} Coins of $1");
         break;
     }
 
